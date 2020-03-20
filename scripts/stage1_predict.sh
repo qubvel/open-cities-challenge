929301e93cc0bff658965a1c4100d9597207a8a6
qubvel <qubvel@gmail.com>
2020-03-20 17:23:50 +0000

    Make Makefile for all stages

--- scripts/stage1_predict.sh
+++
@@ -1,7 +0,0 @@
-python -m src.predict \
-    --configs $(ls configs/stage1*) \
-    --test_dir data/processed/test/ \
-    --test_csv data/preocessed/test_mosaic.csv \
-    --dst_dir data/interim/stage1/ \
-    --batch_size 8 \
-    --gpu '0'
